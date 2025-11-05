685642f7d1ab0dde9a63a83a85a381accb904663
Arun Chhetri <chhetriarun999@gmail.com>
2025-11-04 22:52:52 -0800

    Delete Practice-Questions directory

--- Practice-Questions/infiniteloop.java
+++
@@ -1,9 +0,0 @@
-public class infiniteloop{
-    public static void main(String[] args) {
-        double a=0;
-        while (true){
-            a=a+1;
-            System.out.println(a);
-        }
-    }
-}
