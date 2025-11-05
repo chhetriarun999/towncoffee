2e6c92b2bb84ce4ba2ca54cd0bc4306eb3fc0e4f
Arun Chhetri <chhetriarun999@gmail.com>
2025-11-04 22:49:32 -0800

    Add files via upload

---
+++ Practice-Questions/infiniteloop.java
@@ -0,0 +1,9 @@
+public class infiniteloop{
+    public static void main(String[] args) {
+        double a=0;
+        while (true){
+            a=a+1;
+            System.out.println(a);
+        }
+    }
+}
